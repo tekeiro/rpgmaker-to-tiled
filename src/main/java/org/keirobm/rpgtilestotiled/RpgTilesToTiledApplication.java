18e708e5366af7ac999fbfdd1366045439cdf397
tekeiro <tekeiro@gmail.com>
2018-03-23 08:51:04 +0100

    Initialized project

---
+++ src/main/java/org/keirobm/rpgtilestotiled/RpgTilesToTiledApplication.java
@@ -0,0 +1,30 @@
+package org.keirobm.rpgtilestotiled;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.keirobm.rpgtilestotiled.controllers.MainController;
+
+import java.io.IOException;
+
+public class RpgTilesToTiledApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/main.fxml"));
+        final Parent root = fxmlLoader.load();
+
+        final MainController mainController = fxmlLoader.getController();
+        mainController.passStage(primaryStage);
+
+        primaryStage.setTitle("RPG Tiles To Tiled Map Editor");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
