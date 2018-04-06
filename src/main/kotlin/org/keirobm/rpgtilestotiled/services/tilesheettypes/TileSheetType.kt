f100189605e43b381a7882ff7cb86c65d9df0a41
tekeiro <tekeiro@gmail.com>
2018-04-06 21:28:28 +0200

    Finished implementing tiles sheets types enumeration

--- src/main/kotlin/org/keirobm/rpgtilestotiled/services/tilesheettypes/TileSheetType.kt
+++ src/main/kotlin/org/keirobm/rpgtilestotiled/services/tilesheettypes/TileSheetType.kt
@@ -1,8 +1,13 @@
 package org.keirobm.rpgtilestotiled.services.tilesheettypes
 
-import org.keirobm.rpgtilestotiled.services.autotileprocessers.TileProcesser
-import org.keirobm.rpgtilestotiled.types.Rect
+import org.keirobm.rpgtilestotiled.services.autotileprocessers.*
+import kotlin.reflect.KClass
 
+
+private val autoTile = GenericAutoTileProcesser::class
+private val autoWall = GenericAutoWallProcesser::class
+private val normalTiles64x96 = ExtractTilesFromLargeImage64x96::class
+private val normalTiles64x64 = ExtractTilesFromLargeImage64x64::class
 
 /**
  * Determine all types of TileSheet that RPG Maker
@@ -11,11 +16,37 @@ import org.keirobm.rpgtilestotiled.types.Rect
  */
 enum class TileSheetType
 (
-
+    val rows: Int,
+    val cols: Int,
+    val processers: List<KClass<out TileProcesser>>
 ) {
-
+    A1(4, 8, listOf(
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, normalTiles64x96,
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, normalTiles64x96,
+            autoTile, autoTile, autoTile, normalTiles64x96, autoTile, autoTile, autoTile, normalTiles64x96,
+            autoTile, autoTile, autoTile, normalTiles64x96, autoTile, autoTile, autoTile, normalTiles64x96
+    )),
+    A2(4, 8, listOf(
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile,
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile,
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile,
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile
+    )),
+    A3(4, 8, listOf(
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall,
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall,
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall,
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall
+    )),
+    A4(6, 8, listOf(
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile,
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall,
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile,
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall,
+            autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile, autoTile,
+            autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall, autoWall
+    ))
     ;
 
 
-    abstract val regions: List<Pair<Rect, TileProcesser>>
 }
